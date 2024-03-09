b6a3e0512f8c35dd6eb63a0063b13fe10bef2f2a
xb1145141 <162800466+xb1145141@users.noreply.github.com>
2024-03-09 19:41:54 +0800

    Update GameData.h

--- version/GameData.h
+++ version/GameData.h
@@ -2,32 +2,32 @@
 class GameData
 {
 	public:
-		bool	InfAmmo = false;
+		bool	InfAmmo = true;
 		bool	InfAmmoLegit = false;
-		bool	InfGrenades = false;
+		bool	InfGrenades = true;
 		bool	InfGrenadesLegit = false;
-		bool	InfSyringes = false;
+		bool	InfSyringes = true;
 		bool	InfSyringesLegit = false;
 		bool	InfStratagems = true;
 		bool	InfMissionTime = false;
 		bool	InfStamina = true;
-		bool	InfHealth = false;
-		bool	NoReload = false;
+		bool	InfHealth = true;
+		bool	NoReload = true;
 		bool	MaxResources = false;
 		bool	Add5Resources = true;
 		bool	Recoil = true;
 		bool	InfBackpack = true;
-		bool	InfSpecWeapon = false;
-		bool	NoCannonOverheat = false;
-		bool	NoStasTurretOverHeat = false;
+		bool	InfSpecWeapon = true;
+		bool	NoCannonOverheat = true;
+		bool	NoStasTurretOverHeat = true;
 		bool	JetpackNoCD = false;
 		bool	ShieldNoCD = true;
-		bool	InstantRailGun = false;
+		bool	InstantRailGun = true;
 		bool	ShowAllMapIcons = true;
 		bool	AllStratagems = true;
 		bool	AllEquipment = true;
 		bool	AllArmor = true;
-		bool	Speedhack = false;
+		bool	Speedhack = true;
 
 		bool	OHK = false;
 };
